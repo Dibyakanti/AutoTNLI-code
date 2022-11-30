dcbe4a7b3130b022bb7287e7a0e04c369d27a387
Dibyakanti <dibyakan@iitg.ac.in>
2022-11-30 21:29:27 +0530

    Minor changes in generator bash script

--- sentence_generation/generate.sh
+++ sentence_generation/generate.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-python3 generate_album.py --category "Album"
-python3 generate.py --category "Book"
-python3 generate.py --category "City"
-python3 generate.py --category "Festival"
-python3 generate.py --category "FoodnDrinks"
-python3 generate.py --category "Movie"
-python3 generate.py --category "Organization"
-python3 generate.py --category "Paint"
-python3 generate.py --category "Person"
-python3 generate.py --category "Person"
-python3 generate.py --category "Person"
+python3 generator.py --category_list Album Book City Festival FoodnDrinks Movie Organization Paint Person SportsnEvents University
---
+++ splits/preprocess_standalone.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import math
+import copy
+import random
+import ast
+import json
+from util import *
+
+string = ["city","album","person","movie","book","foodndrinks","organization","paint","festival","sportsnevents","university"]
+table = ["_T0","_F1","_F2","_F3","_F4","_F5"] # Orig = T0 , Counter = table - T0
+
